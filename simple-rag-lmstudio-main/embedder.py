c67bb7ab9f618cd89aeceb748275643541fd0d44
XavBertrand <bertrand.xav@gmail.com>
2024-05-19 15:37:48 +0200

    Improved embedder

--- simple-rag-lmstudio-main/embedder.py
+++ simple-rag-lmstudio-main/embedder.py
@@ -1,43 +1,20 @@
-from langchain_community.embeddings import HuggingFaceBgeEmbeddings, HuggingFaceEmbeddings
-
+from langchain_community.embeddings import (
+    HuggingFaceEmbeddings,
+)
 from langchain_community.vectorstores import Chroma
 from langchain_community.document_loaders import TextLoader, DirectoryLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
-import sys
+import os
 
-# data_directory = "./content"
-# embedding_directory = "./content/chroma_db"
-data_directory = (
-    r"C:\Users\bertr\PycharmProjects\TestRAG\simple-rag-lmstudio-main\content"
-)
-embedding_directory = (
-    r"C:\Users\bertr\PycharmProjects\TestRAG\simple-rag-lmstudio-main\content\chroma_db"
-)
 
 embedding_db = None
 
 
-def embed(embedding_model="dangvantuan/sentence-camembert-base"):
-    print("\nCalculating Embeddings\n")
-
-    # Load the text from the data directory
-    # loader = DirectoryLoader(data_directory,
-    #                          glob="*.txt",
-    #                          loader_cls=TextLoader)
-
-    loader = DirectoryLoader(data_directory, glob="**/*.txt")
-
-    documents = loader.load()
-
-    # Split the data into chunks
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=128)
-
-    chunks = text_splitter.split_documents(documents)
+def embed(embedding_model="dangvantuan/sentence-camembert-base", content_path=None):
+    data_directory = content_path
+    embedding_directory = os.path.join(content_path, "chroma_db")
 
     # Load the huggingface embedding model
-    # model_name = "BAAI/bge-base-en-v1.5"
-    # model_name = "sentence-transformers/all-MiniLM-L6-v2"
-    # model_name = "dangvantuan/sentence-camembert-base"
     model_name = embedding_model
 
     encode_kwargs = {
@@ -47,11 +24,27 @@ def embed(embedding_model="dangvantuan/sentence-camembert-base"):
     # embedding_model = HuggingFaceBgeEmbeddings(
     embedding_model = HuggingFaceEmbeddings(
         model_name=model_name,
-        # model_kwargs={'device': 'cpu'},
         model_kwargs={"device": "cuda"},
+        # model_kwargs={"device": "cpu"},
         encode_kwargs=encode_kwargs,
         show_progress=True,
     )
+
+    print("\nCalculating Embeddings\n")
+
+    # Load the text from the data directory
+    loader = DirectoryLoader(data_directory, glob="**/*.txt")
+
+    documents = loader.load()
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        chunk_overlap=100,
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    print(f"Number of text splits generated: {len(chunks)}")
 
     embedding_db = Chroma.from_documents(
         chunks, embedding_model, persist_directory=embedding_directory
