756f9cd90ddbbfec813c916a23ca624e514ae986
XavBertrand <bertrand.xav@gmail.com>
2024-08-24 19:58:53 +0200

    Inputs in dict

--- simple-rag-lmstudio-main/main.py
+++ simple-rag-lmstudio-main/main.py
@@ -4,39 +4,53 @@ from embedder_raptor import embed_raptor
 from embedder_summary import embed_summary
 from chat import startChat
 import os
+import warnings
+
 
 os.environ["TESSDATA_PREFIX"] = r"C:\\Program Files\\Tesseract-OCR\\tessdata"
 
-# Starting URL
-# start_url = "https://docs.solace.com/Cloud/Event-Portal/event-portal-lp.htm"
-# start_url = "https://pytorch-geometric.readthedocs.io/en/latest/modules/nn.html"
-# Starting depth
-# start_depth = 3
+# todo: add conf in a json file
+config = {
+    "llm_ollama": "Mistral-NeMo-for-RAG:latest",
+    "documents_path": r"C:\Users\bertr\PycharmProjects\TestRAG\data\wetransfer_pieces-we-transfert_2023-10-27_1533",
+    "content_path": r"C:\Users\bertr\PycharmProjects\TestRAG\simple-rag-lmstudio-main\content",
+    "pytesseract_path": r"C:/Program Files/Tesseract-OCR/tesseract.exe",
+    "embedding_model": "Lajavaness/bilingual-embedding-large-8k",
+    "FORCE_SCRAPER": False,
+    "FORCE_EMBED": True,
+    "RAPTOR": True,
+    "EMBED_SUMMARY": False,
+    "chunk_size": 500,
+    "chunk_overlap": 100,
+    "n_levels_raptor": 3,
+    "ui": "gradio",
+}
 
-documents_path = r"C:\Users\bertr\PycharmProjects\TestRAG\data\wetransfer_pieces-we-transfert_2023-10-27_1533"
-content_path = (
-    r"C:\Users\bertr\PycharmProjects\TestRAG\simple-rag-lmstudio-main\content"
-)
 
-pytesseract_path = r"C:/Program Files/Tesseract-OCR/tesseract.exe"
+documents_path = config["documents_path"]
+content_path = config["content_path"]
+pytesseract_path = config["pytesseract_path"]
+embedding_model = config["embedding_model"]
+FORCE_SCRAPER = config["FORCE_SCRAPER"]
+FORCE_EMBED = config["FORCE_EMBED"]
+EMBED_SUMMARY = config["EMBED_SUMMARY"]
+RAPTOR = config["RAPTOR"]
+llm_ollama = config["llm_ollama"]
 
-embedding_model = "dangvantuan/sentence-camembert-base"
-# embedding_model = "dangvantuan/sentence-camembert-large"
-# embedding_model = "Alibaba-NLP/gte-Qwen2-1.5B-instruct"  # Large context
-
-FORCE_SCRAPER = False
 if FORCE_SCRAPER:
-    # scrape(start_url, start_depth)
     extract_txt_from_dir(documents_path, content_path, pytesseract_path)
 
-FORCE_EMBED = False
-RAPTOR = False
 if FORCE_EMBED:
     if RAPTOR:
-        llm_raptor = "Mistral-NeMo-for-RAG:latest"
-        embed_raptor(embedding_model, content_path, llm_raptor)
+        embed_raptor(embedding_model, content_path, config)
+        if EMBED_SUMMARY:
+            warnings.warn("EMBED_SUMMARY not compatible yet with RAPTOR!!")
     else:
-        # embed(embedding_model, content_path)
-        embed_summary(embedding_model, content_path, llm_name="Mistral-NeMo-for-RAG:latest")
+        if EMBED_SUMMARY:
+            embed_summary(
+                embedding_model, content_path, config,
+            )
+        else:
+            embed(embedding_model, content_path, config)
 
-startChat(embedding_model, raptor=RAPTOR)
+startChat(embedding_model, config)
